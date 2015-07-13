f9148193d120b4b4e7856f81414ca19bfb0faeda
Justin Abrahms <justin@abrah.ms>
2015-07-13 12:21:01 -0700

    correct version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='imhotep_eslint',
-    version='0.0.2',
+    version='0.0.1',
     packages=find_packages(),
     url='https://github.com/justinabrahms/imhotep_eslint',
     license='MIT',
