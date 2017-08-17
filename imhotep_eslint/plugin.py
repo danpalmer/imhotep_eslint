f504067548093bc720c3cd2c9d2594302a5688b2
Dan Palmer <dan@danpalmer.me>
2017-08-17 20:39:41 +0100

    Support JSX as well

--- imhotep_eslint/plugin.py
+++ imhotep_eslint/plugin.py
@@ -7,7 +7,7 @@ class ESLint(Tool):
     # Example: foo.js: line 5, col 3, Operator + should not stick ...etc
     response_format = re.compile(r'^(?P<filename>.*): ' \
         r'line (?P<line_number>\d+), col \d+, (?P<message>.*)$')
-    file_extensions = ('.js',)
+    file_extensions = ('.js', '.jsx')
 
     def get_command(self, dirname, linter_configs=None):
         cmd = 'eslint -f compact'
